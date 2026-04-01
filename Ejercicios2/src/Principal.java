8d35633823b1efa4749619b988744197a5a0d3ca
Laboratorio <laboratorio@udla-ec.int>
2026-04-01 12:10:29 -0500

    Segundo envio

--- Ejercicios2/src/Principal.java
+++ Ejercicios2/src/Principal.java
@@ -1,8 +1,11 @@
 public class Principal {
     public static void main(String[] args){
         Estudiante e1=new Estudiante();
+        Estudiante e2=new Estudiante();
         e1.ingresarDatos();
         e1.mostrarDatos();
+        e2.ingresarDatos();
+        e2.mostrarDatos();
     }
 }
 
